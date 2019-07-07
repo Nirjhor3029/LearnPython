a40fcc6de0b9641f234669ae7e2ba7aae25eccc1
Nirjhor3029 <sazzad3029@gmail.com>
2019-07-07 11:44:58 +0600

    upload

---
+++ app.py
@@ -0,0 +1,31 @@
+print("Hello World")
+print('O----')
+print(' |||')
+print('*' * 10)
+
+# variables
+price = 10
+rating = 4.9
+my_name = "Nirjhor"
+is_published = False
+print(price, rating)
+
+# Exercise
+name = "John Smith"
+age = 20
+is_new = True
+print(name, age, is_new)
+
+# 3rd class
+y_name = input('what is your name? ')
+print('Hi '+y_name)
+# Exercise
+name_3 = input('What is your name? ')
+favorite_color = input('What is your favorite color? ')
+print(name_3 + ' likes ' + favorite_color)
+
+# 4th Class
+
+
+
+
